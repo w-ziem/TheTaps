5690ec10e015341b0a64da8e3e7bc4fb862c0219
Wojciech Ziembowicz <143935791+w-ziem@users.noreply.github.com>
2025-06-27 20:31:30 +0200

    connect form to apps script

---
+++ script.js
@@ -0,0 +1,41 @@
+// changing tabs on hash
+
+const sections = document.querySelectorAll('.section');
+
+function showSectionFromHash() {
+    const hash = window.location.hash.replace('#', '') || 'home-page';
+    sections.forEach(s => s.style.display = s.id === hash ? 'block' : 'none');
+}
+
+window.addEventListener('hashchange', showSectionFromHash);
+window.addEventListener('load', showSectionFromHash);
+
+
+// form handling (Apps script)
+const scriptURL = 'https://script.google.com/macros/s/AKfycby3vgRC4e4xoUUdt55SZ7xHk_7saQ31Sj-KAYqPjZnjiB7Xe4jqqFmgHGC6fD1ZIaRCAQ/exec';
+const form = document.getElementById('contact-form');
+const statusElement = document.getElementById('status');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    statusElement.textContent = 'Sending...';
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            console.log('Success!', response);
+            statusElement.textContent = 'Wiadomość została wysłana!';
+            statusElement.style.color = 'green';
+            sleep(2000).then(() => {
+                statusElement.textContent = '';
+            });
+            form.reset();
+        })
+        .catch(error => {
+            console.error('Error!', error.message);
+            statusElement.textContent = 'Błąd wysyłania wiadomości. Spróbuj ponownie.';
+            statusElement.style.color = 'red';
+            sleep(2000).then(() => {
+                statusElement.textContent = '';
+            });
+        });
+});
